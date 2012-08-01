f969475d6c7d980429cad39ed5684c240b310025
Václav Novotný <vaclavnovotny@vaclavnovotny.eu>
2012-08-01 07:30:53 +0200

    Init commit

---
+++ Edge/MailQueueBundle/EdgeMailQueueBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Edge\MailQueueBundle;
+
+/**
+ * Mail queue bundle
+ *
+ * @author VeN <vaclav.novotny@edgedesign.cz>
+ */
+class EdgeMailQueueBundle extends \Symfony\Component\HttpKernel\Bundle\Bundle
+{
+
+}
